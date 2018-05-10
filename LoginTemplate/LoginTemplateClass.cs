25cfa66a406efbabdb6f4da5c4b5705b4487f8ca
Leon <xiaoyankeji@hotmail.com>
2018-05-10 12:56:32 +0800

    增加登录插件模板LoginTemplate；

--- LoginTemplate/LoginTemplateClass.cs
+++ LoginTemplate/LoginTemplateClass.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace StartUpTemplate
+namespace LoginTemplate
 {
-    public abstract class StartUpTemplateClass : IDisposable
+    public abstract class LoginTemplateClass : IDisposable
     {
         /// <summary>
         /// 图标
         /// </summary>
         public static Icon StartUpIcon = null;
+        /// <summary>
+        /// 用户名 (传送至 StartUp 插件)
+        /// </summary>
+        public static string UserName = string.Empty;
+        /// <summary>
+        /// 密码 (传送至 StartUp 插件)
+        /// </summary>
+        public static string Password = string.Empty;
+        /// <summary>
+        /// 头像 (传送至 StartUp 插件)
+        /// </summary>
+        public static Image HeadPortrait = null;
         /// <summary>
         /// 启动名称
         /// </summary>
@@ -26,8 +37,8 @@ namespace StartUpTemplate
         /// 程序集所在文件名称
         /// </summary>
         public abstract string FileName { get; }
-        
-        private Image _preview = StartUpTemplateResource.DefaultStartUpPreview;
+
+        private Image _preview = LoginTemplateResource.DefaultLoginPreview;
         /// <summary>
         /// 启动预览图 (图像尺寸为 160 x 90 px)
         /// </summary>
@@ -37,19 +48,19 @@ namespace StartUpTemplate
             protected set
             {
                 if (value == null)
-                    _preview = StartUpTemplateResource.DefaultStartUpPreview;
+                    _preview = LoginTemplateResource.DefaultLoginPreview;
                 else
                     _preview = new Bitmap(value, 160, 90);
             }
         }
 
         // 使用 volatile 关键字，防止多线程对对象造成不可预期的影响
-        private volatile Form _startUpForm = null;
+        private volatile Form _loginForm = null;
 
         /// <summary>
         /// 启动窗口
         /// </summary>
-        public Form StartUpForm
+        public Form LoginForm
         {
             /* 
              * 不要在子类的构造函数里 new 出 Form，否则会自动创建出 Form 占用很多内存
@@ -57,39 +68,39 @@ namespace StartUpTemplate
              */
             get
             {
-                if (_startUpForm == null)
-                    _startUpForm = CreateStartUpForm();
-                if (_startUpForm != null) _startUpForm.Icon = StartUpIcon;
-                return _startUpForm;
+                if (_loginForm == null)
+                    _loginForm = CreateLoginForm();
+                if (_loginForm != null) _loginForm.Icon = StartUpIcon;
+                return _loginForm;
             }
-            protected set => _startUpForm = value;
+            protected set => _loginForm = value;
         }
 
         /// <summary>
         /// 启动完成事件（用于系统订阅）
         /// </summary>
-        public event EventHandler<EventArgs> StartUpFinished; //{add{}remove{}}
+        public event EventHandler<EventArgs> LoginFinished; //{add{}remove{}}
 
         /// <summary>
-        /// 触发启动完成事件
+        /// 触发登录完成事件
         /// </summary>
         /// <param name="e"></param>
-        public void OnStartUpFinished(EventArgs e)
+        public void OnLoginFinished(EventArgs e)
         {
-            this?.StartUpFinished?.Invoke(this, e);
-            
+            this?.LoginFinished?.Invoke(this, e);
+
             //启动完成后自动释放启动画面内存；
-            _startUpForm?.Dispose();
-            StartUpForm = null;
+            _loginForm?.Dispose();
+            LoginForm = null;
         }
 
         /// <summary>
         /// 构造启动窗口
         /// </summary>
         /// <returns>启动窗口</returns>
-        protected abstract Form CreateStartUpForm();
+        protected abstract Form CreateLoginForm();
 
-        ~StartUpTemplateClass()
+        ~LoginTemplateClass()
         {
             (this as IDisposable).Dispose();
         }
@@ -98,7 +109,7 @@ namespace StartUpTemplate
         {
             System.Diagnostics.Debug.Print("Dispose : {0}", this.Name);
             _preview?.Dispose();
-            _startUpForm?.Dispose();
+            _loginForm?.Dispose();
             GC.SuppressFinalize(this);
         }
 
