0c4ec4e1a6eb8efb2fc857c6873f345f61aebc3b
Leon <Leon.ID@QQ.COM>
2018-05-25 21:18:05 +0800

    增加解密程序；

---
+++ DefaultPorgrams/DecryptClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTemplate;
+using System.Windows.Forms;
+
+namespace DefaultPorgrams
+{
+    public class DecryptClass : ProgramTemplateClass
+    {
+        public DecryptClass()
+        {
+            Name = "解密";
+            Description = "解密 [via leon]";
+            Icon = DefaultProgramResource.DecryptIcon;
+        }
+
+        public override string FileName => System.Reflection.Assembly.GetExecutingAssembly().ManifestModule.ScopeName;
+
+        protected override Form CreateProgramForm()
+        {
+            return new DefaultProgramForm(
+                Name,
+                Icon,
+                DefaultProgramResource.Decrypt
+                );
+        }
+    }
+}
--- DefaultPorgrams/DefaultProgramResource.Designer.cs
+++ DefaultPorgrams/DefaultProgramResource.Designer.cs
@@ -233,6 +233,26 @@ namespace DefaultPorgrams {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap Decrypt {
+            get {
+                object obj = ResourceManager.GetObject("Decrypt", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap DecryptIcon {
+            get {
+                object obj = ResourceManager.GetObject("DecryptIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap DefaultIcon {
             get {
                 object obj = ResourceManager.GetObject("DefaultIcon", resourceCulture);
