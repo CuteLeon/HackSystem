d5ed03a12b1ed421686332f1212d376c30f1e6b7
Leon <xiaoyankeji@hotmail.com>
2018-05-25 18:16:07 +0800

    增加攻击数据程序；

---
+++ DefaultPorgrams/AttackDataClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTemplate;
+using System.Windows.Forms;
+
+namespace DefaultPorgrams
+{
+    public class AttackDataClass : ProgramTemplateClass
+    {
+        public AttackDataClass()
+        {
+            Name = "攻击数据";
+            Description = "攻击数据 [via leon]";
+            Icon = DefaultProgramResource.AttackDataIcon;
+        }
+
+        public override string FileName => System.Reflection.Assembly.GetExecutingAssembly().ManifestModule.ScopeName;
+
+        protected override Form CreateProgramForm()
+        {
+            return new DefaultProgramForm(
+                Name,
+                Icon,
+                DefaultProgramResource.AttackData
+                );
+        }
+    }
+}
--- DefaultPorgrams/DefaultProgramResource.Designer.cs
+++ DefaultPorgrams/DefaultProgramResource.Designer.cs
@@ -143,6 +143,26 @@ namespace DefaultPorgrams {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap AttackData {
+            get {
+                object obj = ResourceManager.GetObject("AttackData", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap AttackDataIcon {
+            get {
+                object obj = ResourceManager.GetObject("AttackDataIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap Close_Down {
             get {
                 object obj = ResourceManager.GetObject("Close_Down", resourceCulture);
