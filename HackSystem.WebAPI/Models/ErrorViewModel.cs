8fab9b31979f29f9c0df9fb6d6fb7cb404a01392
Leon <Leon.Engineer@hotmail.com>
2020-08-29 04:24:34 +0800

    创建 WebAPI 项目；

---
+++ HackSystem.WebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HackSystem.WebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
