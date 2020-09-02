c5cfffe9b865495e923f83ea53a3cce108044d1b
Leon <Leon.Engineer@hotmail.com>
2020-09-03 00:40:36 +0800

    控制器使用DTO；

--- HackSystem.WebAPI/Controllers/BasicProgramController.cs
+++ HackSystem.WebAPI/Controllers/BasicProgramController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using HackSystem.WebAPI.Model.Program;
 using HackSystem.WebAPI.Services.API.DataServices.Program;
+using HackSystem.WebDataTransfer.Program;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -25,42 +27,51 @@ namespace HackSystem.WebAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<BasicProgram>> GetAll()
+        public async Task<IEnumerable<QueryBasicProgramDTO>> GetAll()
         {
             this.logger.LogInformation($"Query all Basic Programs.");
-            return await this.basicProgramDataService.ToArrayAsync();
+            var entities = await this.basicProgramDataService.ToArrayAsync();
+            var result = entities.Select(x => new QueryBasicProgramDTO() { Id = x.Id, Name = x.Name }).ToArray();
+            return result;
         }
 
         [HttpGet]
-        public async Task<BasicProgram> Get(string programId)
+        public async Task<QueryBasicProgramDTO> Get(string programId)
         {
             this.logger.LogInformation($"Query Basic Programs id = {programId}");
-            return await this.basicProgramDataService.FindAsync(programId);
+            var entity = await this.basicProgramDataService.FindAsync(programId);
+            var result = new QueryBasicProgramDTO() { Id = entity.Id, Name = entity.Name };
+            return result;
         }
 
         [HttpPost]
-        public async Task<BasicProgram> Post([FromBody] BasicProgram basicProgram)
+        public async Task<QueryBasicProgramDTO> Create([FromBody] CreateBasicProgramDTO basicProgram)
         {
             this.logger.LogInformation($"Create Basic Programs: {basicProgram.Name}");
-            var entity = await this.basicProgramDataService.AddAsync(basicProgram);
-            return entity;
+            var entity = new BasicProgram() { Name = basicProgram.Name };
+            entity = await this.basicProgramDataService.AddAsync(entity);
+            var result = new QueryBasicProgramDTO() { Id = entity.Id, Name = entity.Name };
+            return result;
         }
 
         [HttpPut]
-        public async Task<BasicProgram> Put([FromBody] BasicProgram basicProgram)
+        public async Task<QueryBasicProgramDTO> Update([FromBody] UpdateBasicProgramDTO basicProgram)
         {
             this.logger.LogInformation($"Update Basic Programs: {basicProgram.Name}");
-            var entity = this.basicProgramDataService.Update(basicProgram);
-            return entity;
+            var entity = new BasicProgram() { Id = basicProgram.Id, Name = basicProgram.Name };
+            entity = this.basicProgramDataService.Update(entity);
+            var result = new QueryBasicProgramDTO() { Id = entity.Id, Name = entity.Name };
+            return result;
         }
 
         [HttpDelete]
-        public async Task<IActionResult> Delete(string programId)
+        public async Task<QueryBasicProgramDTO> Delete(string programId)
         {
             this.logger.LogInformation($"Delete Basic Programs id = {programId}");
             var entity = await this.basicProgramDataService.FindAsync(programId);
             this.basicProgramDataService.Remove(entity);
-            return this.Ok();
+            var result = new QueryBasicProgramDTO() { Id = entity.Id, Name = entity.Name };
+            return result;
         }
     }
 }
---
+++ HackSystem.WebDataTransfer/Program/UpdateBasicProgramDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HackSystem.WebDataTransfer.Program
+{
+    public class UpdateBasicProgramDTO
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
