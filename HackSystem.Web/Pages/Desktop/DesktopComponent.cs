15b625b4c0f57b1f029df9e7d749a60a012c4e46
Leon <Leon.Engineer@hotmail.com>
2020-09-16 01:52:11 +0800

    增加顶部栏组件；优化桌面布局；

--- HackSystem.Web/Pages/Desktop/DesktopComponent.cs
+++ HackSystem.Web/Pages/Desktop/DesktopComponent.cs
@@ -21,16 +21,6 @@ namespace HackSystem.Web.Pages.Desktop
             }
         }
 
-        private async Task OnTest()
-        {
-            this.GetToastContainer().PopToast("Hack System", "此消息将在 1 秒自动关闭。", Icons.Information, true, 1000);
-            this.GetToastContainer().PopToast("Hack System", "此消息将在 2 秒自动关闭。", Icons.Question, true, 2000);
-            this.GetToastContainer().PopToast("Hack System", "此消息将在 3 秒自动关闭。", Icons.Warning, true, 3000);
-            this.GetToastContainer().PopToast("Hack System", "此消息需要处理后手动关闭。", Icons.Error, false);
-            this.GetToastContainer().PopToast("Hack System", "欢迎进入 Hack System。", Icons.HackSystem, false);
-            await this.LoadProgramIcons();
-        }
-
         private async Task LoadProgramIcons()
         {
             try
