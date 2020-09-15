c3ff0b06603f3dc7cd13fc110043b1280f7a4bda
Leon <Leon.Engineer@hotmail.com>
2020-09-16 00:43:41 +0800

    重构桌面布局；图标名称自动省略；加深图标名称文本阴影；实现桌面程序图标自上而下、自左而右布局；实现桌面程序图标响应事件效果；

---
+++ HackSystem.Web/wwwroot/js/hacksystem.programdrawer.js
@@ -0,0 +1,38 @@
+﻿var drawerEvents = {
+    mouseOverIcon: function (e) {
+        let icon = $(e).parent().parent();
+        let focused = icon.is(':focus');
+        if (focused) return;
+
+        icon.addClass('shadow');
+        icon.css('background', 'rgba(255, 255, 255, 0.2)');
+        icon.css('backdrop-filter', 'blur(5px)');
+    },
+    mouseOutIcon: function (e) {
+        let icon = $(e).parent().parent();
+        let focused = icon.is(':focus');
+        if (focused) return;
+
+        icon.removeClass('shadow');
+        icon.css('background', 'rgba(255, 255, 255, 0)');
+        icon.css('backdrop-filter', '');
+    },
+    mouseDownIcon: function (e) {
+        let icon = $(e).parent().parent();
+        icon.css('background', 'rgba(255, 255, 255, 0.5)');
+    },
+    mouseUpIcon: function (e) {
+        let icon = $(e).parent().parent();
+        icon.css('background', 'rgba(255, 255, 255, 0.2)');
+    },
+    focusIcon: function (e) {
+        let icon = $(e);
+        icon.css('background', 'rgba(255, 255, 255, 0.2)');
+        icon.css('backdrop-filter', 'blur(5px)');
+    },
+    blurIcon: function (e) {
+        let icon = $(e);
+        icon.css('background', 'rgba(255, 255, 255, 0)');
+        icon.css('backdrop-filter', '');
+    }
+};
