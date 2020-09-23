b7a7ba46e8f5336b6244539d7db0e258c2c09138
Leon <Leon.Engineer@hotmail.com>
2020-09-23 21:32:42 +0800

    封装 Resizeable 视图为组件；

---
+++ HackSystem.Web/Shared/Resizeable/ResizeableOption.cs
@@ -0,0 +1,15 @@
+﻿namespace HackSystem.Web.Shared.Resizeable
+{
+    public class ResizeableOption
+    {
+        public int Left { get; set; } = 0;
+
+        public int Top { get; set; } = 0;
+
+        public string ResizeTarget { get; set; } = ".position-fixed";
+
+        public int BorderSize { get; set; } = 8;
+
+        public int CornerSize { get; set; } = 16;
+    }
+}
