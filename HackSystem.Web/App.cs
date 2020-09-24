6abd5c891269898d4c33e5fac8fec9f1c9f542fc
Leon <Leon.Engineer@hotmail.com>
2020-09-25 02:28:57 +0800

    Prepare for Lazy Load.

---
+++ HackSystem.Web/App.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.Extensions.Logging;
+
+namespace HackSystem.Web
+{
+    public partial class App
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            this.logger.LogInformation($"Initialize App...");
+            await base.OnInitializedAsync();
+        }
+
+        protected async Task OnNavigateAsync(NavigationContext navigationContext)
+        {
+            this.logger.LogInformation($"Navigating to {navigationContext.Path}");
+        }
+    }
+}
