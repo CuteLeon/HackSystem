6ceaab3309407dcfddbedb9863b4a60bfc3232ba
Leon <Leon.Engineer@hotmail.com>
2021-02-09 00:44:57 +0800

    Use batch command to copy blazor.webassembly.js automacticly;

--- HackSystem.Web/wwwroot/js/hacksystem.startupprogress.js
+++ HackSystem.Web/wwwroot/js/hacksystem.startupprogress.js
@@ -1,4 +1,5 @@
 ﻿/* Have to modify {PublishDir}/wwwroot/_framework/blazor.webassembly.js file Manually!
+e.prototype.loadResources = function (e, t, n) {var r = this;var resourcesCount = Object.keys(e).length, index = 0;console.log(`Blazor WebAssembly => Loading ${resourcesCount} resources...`);return Object.keys(e).map((function (o) {var p = r.loadResource(o, t(o), e[o], n);p.response.then((x) => {if (typeof window.loadResourceCallback === "function") {window.loadResourceCallback(++index, resourcesCount, o);}});return p;}))},
 e.prototype.loadResources = function (e, t, n) {
     var r = this;
     var resourcesCount = Object.keys(e).length, index = 0; // Added new logic
