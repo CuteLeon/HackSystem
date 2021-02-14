b5ca49b25934285be2a99d74828f9493f2010553
Leon <Leon.Engineer@hotmail.com>
2021-02-15 00:49:27 +0800

    Update nuget packages;

--- HackSystem.Web/Program.cs
+++ HackSystem.Web/Program.cs
@@ -59,7 +59,7 @@ namespace HackSystem.Web
                 .AddAuthorizationCore()
                 .AddHackSystemAuthentication(options =>
                 {
-                    options.AnonymousState.User.Claims.Append(new Claim(ClaimTypes.Name, "Anonymous"));
+                    _ = options.AnonymousState.User.Claims.Append(new Claim(ClaimTypes.Name, "Anonymous"));
                     options.AuthenticationURL = apiConfiguration.APIURL;
                     options.TokenExpiryInMinutes = apiConfiguration.TokenExpiryInMinutes;
                     options.AuthenticationScheme = WebCommonSense.AuthenticationScheme;
