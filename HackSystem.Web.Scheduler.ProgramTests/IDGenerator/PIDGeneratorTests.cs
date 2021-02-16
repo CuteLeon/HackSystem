b47b946e0e0fbecb3d1b4e745c8643329f768c55
Leon <Leon.Engineer@hotmail.com>
2021-02-16 17:50:11 +0800

    UnitTests

---
+++ HackSystem.Web.Scheduler.ProgramTests/IDGenerator/PIDGeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackSystem.Web.Scheduler.Program.IDGenerator.Tests
+{
+    public class PIDGeneratorTests
+    {
+        [Fact()]
+        public void GetAvailablePIDTest()
+        {
+            IPIDGenerator generator = new PIDGenerator();
+            var pidPool = new HashSet<int>();
+
+            Parallel.For(0, 10000, x =>
+            {
+                var pid = generator.GetAvailablePID();
+                lock (pidPool)
+                {
+                    Assert.DoesNotContain(pid, pidPool);
+                    pidPool.Add(pid);
+                }
+            });
+
+            Assert.Equal(10000, pidPool.Count);
+        }
+    }
+}
