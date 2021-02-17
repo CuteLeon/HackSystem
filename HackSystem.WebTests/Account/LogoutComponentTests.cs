b5102d22608b526f663ffcb47dcc49a7dfb741a3
Leon <Leon.Engineer@hotmail.com>
2021-02-17 11:55:36 +0800

    Logout component Unit Test: Mock HttpClient request and Mock Navigation manager;

--- HackSystem.WebTests/Account/LogoutComponentTests.cs
+++ HackSystem.WebTests/Account/LogoutComponentTests.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 using Bunit;
 using HackSystem.Web.Account;
 using HackSystem.Web.Authentication.Extensions;
 using HackSystem.Web.CookieStorage;
 using HackSystem.Web.Services.API.Authentication;
 using HackSystem.Web.Services.Authentication;
+using HackSystem.WebTests.Mocks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Moq.Contrib.HttpClient;
 using Xunit;
 
 namespace HackSystem.WebTests.Account
@@ -15,18 +21,30 @@ namespace HackSystem.WebTests.Account
         [Fact()]
         public void LogoutComponentTest()
         {
-            const string AuthenticationURL = "https://localhost:4237";
+            Uri baseUri = new Uri("https://localhost:4237/");
+            Uri logoutUri = new Uri(baseUri, "/api/accounts/logout");
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            var mockHttpClient = mockHttpMessageHandler.CreateClient();
+            mockHttpMessageHandler.SetupAnyRequest().Throws(new HttpRequestException("Not allowed request than testing request."));
+            mockHttpMessageHandler.SetupRequest(HttpMethod.Get, logoutUri).ReturnsResponse(HttpStatusCode.OK, "Logout successfully.");
+            mockHttpClient.BaseAddress = baseUri;
+            var mockNavigationManager = new MockNavigationManager(baseUri.AbsoluteUri, logoutUri.AbsoluteUri);
+
             using var ctx = new TestContext();
-            ctx.Services.AddLogging()
-                .AddCookieStorage()
-                .AddAuthorizationCore()
+            ctx.Services
+                .AddLogging()
+                .AddSingleton<NavigationManager>(mockNavigationManager)
+                .AddSingleton(new Mock<ICookieStorageService>().Object)
                 .AddHackSystemAuthentication(options =>
                 {
-                    options.AuthenticationURL = AuthenticationURL;
+                    options.AuthenticationURL = baseUri.AbsoluteUri;
                 })
-                .AddHttpClient<IAuthenticationService, AuthenticationService>(httpClient => httpClient.BaseAddress = new Uri(AuthenticationURL));
+                .AddScoped<IAuthenticationService, AuthenticationService>()
+                .AddSingleton(mockHttpClient);
 
             using var logoutComponent = ctx.RenderComponent<LogoutComponent>();
+            logoutComponent.WaitForState(() => mockNavigationManager.HasNavigated, TimeSpan.FromSeconds(5));
+            Assert.EndsWith("/StartUp", mockNavigationManager.Uri, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
---
+++ HackSystem.WebTests/Mocks/MockNavigationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace HackSystem.WebTests.Mocks
+{
+    public class MockNavigationManager : NavigationManager
+    {
+        public MockNavigationManager(string baseUri, string uri)
+        {
+            this.Initialize(baseUri, uri);
+        }
+
+        public List<string> NavigationHistory { get; init; } = new List<string>();
+
+        public bool HasNavigated { get => this.NavigationHistory.Any(); }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            this.Uri = this.ToAbsoluteUri(uri).AbsoluteUri;
+            this.NavigationHistory.Add(uri);
+        }
+    }
+}
