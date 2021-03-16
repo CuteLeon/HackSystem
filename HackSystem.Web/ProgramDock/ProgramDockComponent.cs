bf73005872db9cf3378c80a07bdb9a05b3a6753e
Leon <Leon.Engineer@hotmail.com>
2021-03-16 22:01:46 +0800

    Lazy load static JavaScript file: programdock, draggable;

--- HackSystem.Web/ProgramDock/ProgramDockComponent.cs
+++ HackSystem.Web/ProgramDock/ProgramDockComponent.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using HackSystem.WebDataTransfer.Program;
+using Microsoft.JSInterop;
 
 namespace HackSystem.Web.ProgramDock
 {
@@ -20,6 +22,16 @@ namespace HackSystem.Web.ProgramDock
                 this.BasicProgramMaps.Add(map.BasicProgram.Id, map);
             }
             this.StateHasChanged();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                await this.jsRuntime.InvokeVoidAsync("blazorJSTools.importJavaScript", "./js/hacksystem.programdock.js");
+            }
         }
     }
 }
---
+++ HackSystem.Web/wwwroot/js/blazor.jsTools.js
@@ -0,0 +1,24 @@
+﻿window.blazorJSTools = {
+    loaded: [],
+    importJavaScript: function (url) {
+        if (blazorJSTools.loaded[url]) {
+            console.log(`script ${url} already loaded.`);
+            return;
+        }
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = url;
+        script.onload = function () {
+            blazorJSTools.loaded[url] = true;
+            console.log(`load script ${url} successfully.`);
+        };
+        script.onerror = function () {
+            console.log(`load script ${url} failed.`);
+            reject(scriptPath);
+        }
+        document.body.appendChild(script);
+        console.log(`loading script ${url}.`);
+    }
+}
