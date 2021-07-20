6348a09b8b7ad4bc037d64a13b9cf50793b6b784
Leon Liu <leon.liu@hotmail.com>
2021-07-20 22:35:46 +0800

    Implement Mock Route Data Service and Unit Test;

---
+++ HackSystem.WebAPI.MockServerTests/DataServices/MockRouteDataServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+using HackSystem.WebAPI.MockServers.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackSystem.WebAPI.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using HackSystem.WebAPI.Model.Mock;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace HackSystem.WebAPI.MockServers.DataServices.Tests
+{
+    public class MockRouteDataServiceTests
+    {
+        [Fact()]
+        public void QueryMockRouteTest()
+        {
+            var mockRoutes = new List<MockRouteDetail>()
+            {
+                new MockRouteDetail() { Enabled = true, MockURI = "/Mock", ResponseBodyTemplate = "URI_Only" },
+                new MockRouteDetail() { Enabled = true, MockURI = "/Mock", MockMethod = "Get", ResponseBodyTemplate = "URI_Get_Only" },
+                new MockRouteDetail() { Enabled = true, MockURI = "/Mock", MockMethod = "Get", MockSourceHost = "localhost", ResponseBodyTemplate = "URI_Get_localhost_Only" },
+                new MockRouteDetail() { Enabled = true, MockURI = "/Mock", MockMethod = "Get", MockSourceHost = "192.168.", ResponseBodyTemplate = "URI_Get_192_168_Only" },
+                new MockRouteDetail() { Enabled = true, MockURI = "/Mock", MockMethod = "Post", ResponseBodyTemplate = "URI_Post_Only" },
+            };
+            var dbContext = new HackSystemDBContext(
+                new DbContextOptionsBuilder<HackSystemDBContext>()
+                .UseLazyLoadingProxies()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+
+            dbContext.AddRange(mockRoutes);
+            dbContext.SaveChanges();
+
+            var count = dbContext.Set<MockRouteDetail>().Count();
+            Assert.Equal(mockRoutes.Count, count);
+
+            var logger = new Mock<ILogger<MockRouteDataService>>();
+            logger.Setup(l => l.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<object>(), It.IsAny<Exception?>(), It.IsAny<Func<object, Exception?, string>>())).Verifiable();
+            IMockRouteDataService mockRouteDataService = new MockRouteDataService(logger.Object, dbContext);
+
+            Assert.ThrowsAsync<ArgumentNullException>(() => mockRouteDataService.QueryMockRoute(string.Empty, "Get", "localhost"));
+            Assert.ThrowsAsync<ArgumentNullException>(() => mockRouteDataService.QueryMockRoute("/Mock", string.Empty, "localhost"));
+            Assert.ThrowsAsync<ArgumentNullException>(() => mockRouteDataService.QueryMockRoute("/Mock", "Get", string.Empty));
+
+            var mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Get", "otherHost").Result;
+            Assert.Equal("URI_Get_Only", mockRoute.ResponseBodyTemplate);
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Get", "localhost").Result;
+            Assert.Equal("URI_Get_localhost_Only", mockRoute.ResponseBodyTemplate);
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Get", "192.168.10.100").Result;
+            Assert.Equal("URI_Get_192_168_Only", mockRoute.ResponseBodyTemplate);
+
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Post", "otherHost").Result;
+            Assert.Equal("URI_Post_Only", mockRoute.ResponseBodyTemplate);
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Post", "localhost").Result;
+            Assert.Equal("URI_Post_Only", mockRoute.ResponseBodyTemplate);
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Post", "192.168.10.100").Result;
+            Assert.Equal("URI_Post_Only", mockRoute.ResponseBodyTemplate);
+
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Put", "otherHost").Result;
+            Assert.Equal("URI_Only", mockRoute.ResponseBodyTemplate);
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Put", "localhost").Result;
+            Assert.Equal("URI_Only", mockRoute.ResponseBodyTemplate);
+            mockRoute = mockRouteDataService.QueryMockRoute("/Mock/", "Put", "192.168.10.100").Result;
+            Assert.Equal("URI_Only", mockRoute.ResponseBodyTemplate);
+        }
+    }
+}
