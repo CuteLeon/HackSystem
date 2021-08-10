0c1b7df1b0625f7d0371fd39e4a148fff4755327
Leon Liu <leon.liu@hotmail.com>
2021-08-10 20:29:06 +0800

    Fix Unit Test;

--- HackSystem.WebAPI.ServicesTests/Options/GenericOptionDataServiceTests.cs
+++ HackSystem.WebAPI.ServicesTests/Options/GenericOptionDataServiceTests.cs
@@ -16,9 +16,9 @@ namespace HackSystem.WebAPI.Services.Options.Tests
         {
             var genericOptions = new List<GenericOption>()
             {
-                new GenericOption() { OptionName = "OptionName", OptionValue = "OptionValue" },
-                new GenericOption() { OwnerLevel = "HackSystem", OptionName = "OptionName", OptionValue = "HackSystemOptionValue" },
-                new GenericOption() { OwnerLevel = "HackSystem", Category = "Generic", OptionName = "OptionName", OptionValue = "HackSystemGenericOptionValue" },
+                new GenericOption() { OptionName = "OptionName", OptionValue = "OptionValue_0" },
+                new GenericOption() { OptionName = "OptionName", OptionValue = "OptionValue_1", OwnerLevel = "HackSystem",},
+                new GenericOption() { OptionName = "OptionName", OptionValue = "OptionValue_2", OwnerLevel = "HackSystem", Category = "Generic" },
             };
             var dbContext = new HackSystemDBContext(
                 new DbContextOptionsBuilder<HackSystemDBContext>()
@@ -41,29 +41,13 @@ namespace HackSystem.WebAPI.Services.Options.Tests
             IGenericOptionDataService genericOptionDataService = new GenericOptionDataService(logger.Object, GetDBContext());
 
             var optionValue = genericOptionDataService.QueryGenericOptions("OptionName").Result;
-            Assert.Equal("OptionValue", optionValue.OptionValue);
-        }
+            Assert.Equal("OptionValue_0", optionValue.OptionValue);
 
-        [Fact()]
-        public void QueryGenericOptionsByOwnerAndCategoryAndNameTest()
-        {
-            var logger = new Mock<ILogger<GenericOptionDataService>>();
-            logger.Setup(l => l.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<object>(), It.IsAny<Exception?>(), It.IsAny<Func<object, Exception?, string>>())).Verifiable();
-            IGenericOptionDataService genericOptionDataService = new GenericOptionDataService(logger.Object, GetDBContext());
+            optionValue = genericOptionDataService.QueryGenericOptions("OptionName", "HackSystem").Result;
+            Assert.Equal("OptionValue_1", optionValue.OptionValue);
 
-            var optionValue = genericOptionDataService.QueryGenericOptions("HackSystem", "Generic", "OptionName").Result;
-            Assert.Equal("HackSystemGenericOptionValue", optionValue.OptionValue);
-        }
-
-        [Fact()]
-        public void QueryGenericOptionsByOwnerAndNameTest()
-        {
-            var logger = new Mock<ILogger<GenericOptionDataService>>();
-            logger.Setup(l => l.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<object>(), It.IsAny<Exception?>(), It.IsAny<Func<object, Exception?, string>>())).Verifiable();
-            IGenericOptionDataService genericOptionDataService = new GenericOptionDataService(logger.Object, GetDBContext());
-
-            var optionValue = genericOptionDataService.QueryGenericOptions("HackSystem", "OptionName").Result;
-            Assert.Equal("HackSystemOptionValue", optionValue.OptionValue);
+            optionValue = genericOptionDataService.QueryGenericOptions("OptionName", "HackSystem", "Generic").Result;
+            Assert.Equal("OptionValue_2", optionValue.OptionValue);
         }
     }
 }
