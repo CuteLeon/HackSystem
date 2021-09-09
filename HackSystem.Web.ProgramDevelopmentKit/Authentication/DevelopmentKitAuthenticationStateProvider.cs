547554951588c80344ce9b1244fd86763979f8ce
Leon Liu <leon.liu@hotmail.com>
2021-09-10 01:47:24 +0800

    Modify DevelopmentKitAuthenticationStateProvider;

--- HackSystem.Web.ProgramDevelopmentKit/Authentication/DevelopmentKitAuthenticationStateProvider.cs
+++ HackSystem.Web.ProgramDevelopmentKit/Authentication/DevelopmentKitAuthenticationStateProvider.cs
@@ -1,11 +1,14 @@
 ﻿using System.Security.Claims;
+using HackSystem.Web.Authentication.Providers;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace HackSystem.Web.ProgramDevelopmentKit.Authentication;
 
-public class DevelopmentKitAuthenticationStateProvider : AuthenticationStateProvider, IDevelopmentKitAuthenticationStateProvider
+public class DevelopmentKitAuthenticationStateProvider : AuthenticationStateProvider, IHackSystemAuthenticationStateProvider
 {
     public AuthenticationState AuthenticationState { get; set; }
+
+    public string Token { get; set; }
 
     public DevelopmentKitAuthenticationStateProvider()
     {
@@ -22,10 +25,32 @@ public class DevelopmentKitAuthenticationStateProvider : AuthenticationStateProv
             new Claim("aud", "https://localhost"),
         };
         this.AuthenticationState = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, authenticationType)));
+
+        this.Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiTGVvbiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6Imxlb25AaGFjay5jb20iLCJQcm9mZXNzaW9uYWwiOiJ0cnVlIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkhhY2tlciIsIkNvbW1hbmRlciJdLCJleHAiOjI1MzQwMjI3MjAwMCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3QiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdCJ9.umAjNPQpwDprNQZFSPSTRFdP0CVtC9KqLU4n5c5a2Lk";
     }
 
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         return this.AuthenticationState;
+    }
+
+    public async ValueTask<string> GetCurrentTokenAsync()
+    {
+        return this.Token;
+    }
+
+    public void NotifyAuthenticationStateChanged(AuthenticationState authenticationState)
+    {
+        throw new NotSupportedException();
+    }
+
+    public ClaimsIdentity ParseClaimsIdentity(string token)
+    {
+        throw new NotSupportedException();
+    }
+
+    public bool CheckClaimsIdentity(ClaimsIdentity claimsIdentity)
+    {
+        throw new NotSupportedException();
     }
 }
--- HackSystem.Web.ProgramDevelopmentKit/Authentication/IDevelopmentKitAuthenticationStateProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-
-namespace HackSystem.Web.ProgramDevelopmentKit.Authentication;
-
-public interface IDevelopmentKitAuthenticationStateProvider
-{
-    AuthenticationState AuthenticationState { get; set; }
-}
