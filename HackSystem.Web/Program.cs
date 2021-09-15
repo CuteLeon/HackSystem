5e834ca3f38a8007c0d66ab85c70b475498c9815
Leon Liu <leon.liu@hotmail.com>
2021-09-15 23:20:35 +0800

    Modify;

--- HackSystem.Web/Program.cs
+++ HackSystem.Web/Program.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using HackSystem.Common;
 using HackSystem.Cryptography;
 using HackSystem.Observer;
+using HackSystem.Web;
 using HackSystem.Web.Authentication.Extensions;
 using HackSystem.Web.Common;
 using HackSystem.Web.Configurations;
@@ -13,96 +13,73 @@ using HackSystem.Web.Extensions;
 using HackSystem.Web.ProgramSchedule;
 using HackSystem.Web.Services.Configurations;
 using HackSystem.Web.Services.Extensions;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace HackSystem.Web;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var env = builder.HostEnvironment;
+var config = builder.Configuration;
+var apiConfiguration = config.GetSection("APIConfiguration").Get<APIConfiguration>();
+var securityConfiguration = config.GetSection("SecurityConfiguration").Get<SecurityConfiguration>();
 
-public static class Program
-{
-    public static async Task Main(string[] args)
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Configuration
+    .AddJsonFile("appsettings.json", true, false)
+    .AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", true, false);
+
+builder.Services
+    .AddAutoMapper(typeof(Program).Assembly)
+    .AddLogging()
+    .AddHackSystemObserver()
+    .AddBlazoredLocalStorage()
+    .AddHackSystemProgramScheduler(options =>
     {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.Configuration
-            .AddJsonFile("appsettings.json", true, false)
-            .AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", true, false);
-        builder.RootComponents.Add<App>("#app");
-
-        builder
-            .InitializeBasicService()
-            .InitializeAuthorizationPolicy();
-
-        await builder
-            .Build()
-            .LaunchBasicServices()
-            .RunAsync();
-    }
-
-    public static WebAssemblyHostBuilder InitializeBasicService(this WebAssemblyHostBuilder builder)
+        options.ProgramLayerStart = 200;
+        options.TopProgramLayerStart = 850;
+    })
+    .AddRSACryptography(options =>
     {
-        var apiConfiguration = builder.Configuration.GetSection("APIConfiguration").Get<APIConfiguration>();
-        var securityConfiguration = builder.Configuration.GetSection("SecurityConfiguration").Get<SecurityConfiguration>();
-        builder.Services
-            .AddAutoMapper(typeof(Program).Assembly)
-            .AddLogging()
-            .AddHackSystemObserver()
-            .AddBlazoredLocalStorage()
-            .AddHackSystemProgramScheduler(options =>
-            {
-                options.ProgramLayerStart = 200;
-                options.TopProgramLayerStart = 850;
-            })
-            .AddRSACryptography(options =>
-            {
-                options.RSAKeyParameters = securityConfiguration.RSAPublicKey;
-            })
-            .AddWebServices(new WebServiceOptions()
-            {
-                APIHost = apiConfiguration.APIHost,
-            })
-            .AddCookieStorage()
-            .AddAuthorizationCore()
-            .AddHackSystemAuthentication(options =>
-            {
-                options.AuthenticationURL = apiConfiguration.APIHost;
-                options.TokenExpiryInMinutes = apiConfiguration.TokenExpiryInMinutes;
-                options.AuthenticationScheme = WebCommonSense.AuthenticationScheme;
-                options.AuthenticationType = WebCommonSense.AuthenticationType;
-                options.AuthTokenName = WebCommonSense.AuthTokenName;
-                options.ExpiryClaimType = WebCommonSense.ExpiryClaimType;
-                options.TokenRefreshInMinutes = apiConfiguration.TokenRefreshInMinutes;
-            })
-            .AddTransient(sp => new HttpClient { BaseAddress = new Uri(apiConfiguration.APIHost) })
-            .AddHttpClient("LocalHttpClient", httpClient => httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-
-        return builder;
-    }
-
-    /// <summary>
-    /// Initialize Authorization Policy
-    /// </summary>
-    /// <param name="builder"></param>
-    /// <returns></returns>
-    public static WebAssemblyHostBuilder InitializeAuthorizationPolicy(this WebAssemblyHostBuilder builder)
+        options.RSAKeyParameters = securityConfiguration.RSAPublicKey;
+    })
+    .AddWebServices(new WebServiceOptions()
     {
-        builder.Services.AddAuthorizationCore(options =>
+        APIHost = apiConfiguration.APIHost,
+    })
+    .AddCookieStorage()
+    .AddAuthorizationCore()
+    .AddHackSystemAuthentication(options =>
+    {
+        options.AuthenticationURL = apiConfiguration.APIHost;
+        options.TokenExpiryInMinutes = apiConfiguration.TokenExpiryInMinutes;
+        options.AuthenticationScheme = WebCommonSense.AuthenticationScheme;
+        options.AuthenticationType = WebCommonSense.AuthenticationType;
+        options.AuthTokenName = WebCommonSense.AuthTokenName;
+        options.ExpiryClaimType = WebCommonSense.ExpiryClaimType;
+        options.TokenRefreshInMinutes = apiConfiguration.TokenRefreshInMinutes;
+    })
+    .AddTransient(sp => new HttpClient { BaseAddress = new Uri(apiConfiguration.APIHost) })
+    .AddAuthorizationCore(options =>
+    {
+        options.AddPolicy(WebCommonSense.AuthorizationPolicy.HackerPolicy, policy =>
         {
-            options.AddPolicy(WebCommonSense.AuthorizationPolicy.HackerPolicy, policy =>
-            {
-                policy.RequireRole(CommonSense.Roles.HackerRole);
-            });
-            options.AddPolicy(WebCommonSense.AuthorizationPolicy.ProfessionalHackerPolicy, policy =>
-            {
-                policy.RequireRole(CommonSense.Roles.HackerRole);
-                policy.RequireClaim(CommonSense.Claims.ProfessionalClaim, "true", "TRUE", "True");
-            });
-            options.AddPolicy(WebCommonSense.AuthorizationPolicy.LeonPolicy, policy =>
-            {
-                policy.RequireUserName("Leon");
-            });
+            policy.RequireRole(CommonSense.Roles.HackerRole);
         });
+        options.AddPolicy(WebCommonSense.AuthorizationPolicy.ProfessionalHackerPolicy, policy =>
+        {
+            policy.RequireRole(CommonSense.Roles.HackerRole);
+            policy.RequireClaim(CommonSense.Claims.ProfessionalClaim, "true", "TRUE", "True");
+        });
+        options.AddPolicy(WebCommonSense.AuthorizationPolicy.LeonPolicy, policy =>
+        {
+            policy.RequireUserName("Leon");
+        });
+    })
+    .AddHttpClient("LocalHttpClient", httpClient => httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
-        return builder;
-    }
-}
+await builder
+    .Build()
+    .LaunchBasicServices()
+    .RunAsync();
