2b18d8201f8fcdec45cfab941b97797e9271b4f6
Leon <Leon.Engineer@hotmail.com>
2021-09-17 22:03:45 +0800

    ImplicitUsings

--- HackSystem.WebHost/Program.cs
+++ HackSystem.WebHost/Program.cs
@@ -1,9 +1,4 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment;
