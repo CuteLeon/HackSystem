25a25531acedcf63adc4eb89b76d963a73d8d1da
Leon Liu <leon.liu@hotmail.com>
2021-09-25 16:12:46 +0800

    Add ProgramUserRepository;

---
+++ HackSystem.WebAPI.ProgramServer.Application/Repository/IProgramUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using HackSystem.WebAPI.Application.Repository.Abstractions;
+using HackSystem.WebAPI.ProgramServer.Domain.Entity;
+
+namespace HackSystem.WebAPI.ProgramServer.Application.Repository
+{
+    public interface IProgramUserRepository : IRepositoryBase<ProgramUser>
+    {
+    }
+}
---
+++ HackSystem.WebAPI.ProgramServer.Infrastructure/Repository/ProgramUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using HackSystem.WebAPI.Application.Repository.Abstractions;
+using HackSystem.WebAPI.ProgramServer.Application.Repository;
+using HackSystem.WebAPI.ProgramServer.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackSystem.WebAPI.ProgramServer.Infrastructure.Repository;
+
+public class ProgramUserRepository : RepositoryBase<ProgramUser>, IProgramUserRepository
+{
+    public ProgramUserRepository(
+        ILogger<ProgramUserRepository> logger,
+        DbContext dbContext)
+        : base(logger, dbContext)
+    {
+    }
+}
