e676e9665065439e28212fd19077d9000f5aef26
Leon <Leon.Engineer@hotmail.com>
2021-09-26 14:09:55 +0800

    Move intermediary handler extension logic to abstractions project;

--- HackSystem.Intermediary/Extensions/HackSystemIntermediaryExtension.cs
+++ HackSystem.Intermediary/Extensions/HackSystemIntermediaryExtension.cs
@@ -1,5 +1,4 @@
 ﻿using HackSystem.Intermediary.Application;
-using HackSystem.Intermediary.Domain;
 using HackSystem.Intermediary.Infrastructure;
 
 namespace HackSystem.Intermediary.Extensions;
@@ -13,31 +12,4 @@ public static class HackSystemIntermediaryExtension
             .AddTransient<IIntermediaryNotificationPublisher, IntermediaryNotificationPublisher>()
             .AddTransient<IIntermediaryCommandSender, IntermediaryCommandSender>()
             .AddTransient<IIntermediaryRequestSender, IntermediaryRequestSender>();
-
-    public static IServiceCollection AddHackSystemNotificationHandler<TNotificationHandler, TNotification>(
-        this IServiceCollection services,
-        ServiceLifetime lifetime = ServiceLifetime.Transient)
-        where TNotificationHandler : IIntermediaryNotificationHandler<TNotification>
-        where TNotification : IIntermediaryNotification
-    {
-        services.Add(new ServiceDescriptor(typeof(INotificationHandler<TNotification>), typeof(TNotificationHandler), lifetime));
-        return services;
-    }
-
-    public static IServiceCollection AddHackSystemCommandHandler<TCommandHandler, TCommand>(
-        this IServiceCollection services,
-        ServiceLifetime lifetime = ServiceLifetime.Transient)
-        where TCommandHandler : IIntermediaryRequestHandler<TCommand, ValueTuple>
-        where TCommand : IIntermediaryRequest<ValueTuple>
-        => services.AddHackSystemRequestHandler<TCommandHandler, TCommand, ValueTuple>(lifetime);
-
-    public static IServiceCollection AddHackSystemRequestHandler<TRequestHandler, TRequest, TResponse>(
-        this IServiceCollection services,
-        ServiceLifetime lifetime = ServiceLifetime.Transient)
-        where TRequestHandler : IIntermediaryRequestHandler<TRequest, TResponse>
-        where TRequest : IIntermediaryRequest<TResponse>
-    {
-        services.Add(new ServiceDescriptor(typeof(IRequestHandler<TRequest, TResponse>), typeof(TRequestHandler), lifetime));
-        return services;
-    }
 }
