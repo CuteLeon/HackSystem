560e54f066264b06a4a92ca142ec1a13fa244f7e
Leon Liu <leon.liu@hotmail.com>
2021-10-09 23:51:07 +0800

    Wrap ToastHandler to raise toast event;

---
+++ HackSystem.Web.Component.Infrastructure/ToastContainer/Handlers/ToastHandler.cs
@@ -0,0 +1,23 @@
+﻿using HackSystem.Intermediary.Application;
+
+namespace HackSystem.Web.Component.ToastContainer;
+
+public class ToastHandler : IToastHandler
+{
+    private readonly ILogger<ToastHandler> logger;
+    private readonly IIntermediaryEventPublisher eventPublisher;
+
+    public ToastHandler(
+        ILogger<ToastHandler> logger,
+        IIntermediaryEventPublisher eventPublisher)
+    {
+        this.logger = logger;
+        this.eventPublisher = eventPublisher;
+    }
+
+    public async Task PopupToast(ToastDetail toastDetail)
+    {
+        this.logger.LogInformation($"Popup toast: {toastDetail.Title}...");
+        await this.eventPublisher.Publish(new ToastEvent { ToastDetail = toastDetail });
+    }
+}
