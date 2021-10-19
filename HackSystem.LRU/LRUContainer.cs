46cdcac6138717e7aeb55d5e4ed4877acd4ca543
Leon Liu <leon.liu@hotmail.com>
2021-10-19 11:55:46 +0800

    Implement LRU cache container;

---
+++ HackSystem.LRU/LRUContainer.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Concurrent;
+
+namespace HackSystem.LRU;
+
+public class LRUContainer<TValue> : LRUContainer<TValue, TValue>
+    where TValue : notnull
+{
+    public LRUContainer()
+        : base(new Func<TValue, TValue>(value => value))
+    {
+    }
+}
+
+public class LRUContainer<TKey, TValue>
+    where TKey : notnull
+{
+    protected Func<TValue, TKey> KeySelector { get; init; }
+    protected ConcurrentDictionary<TKey, LRUNode<TValue>> Nodes { get; init; } = new();
+
+    public TValue? HeadValue { get => this.Head is null ? default : this.Head!.Value; }
+    public TValue? TailValue { get => this.Tail is null ? default : this.Tail!.Value; }
+
+    protected LRUNode<TValue>? Head { get; set; }
+    protected LRUNode<TValue>? Tail { get; set; }
+
+    public LRUContainer(Func<TValue, TKey> keySelector)
+    {
+        this.KeySelector = keySelector;
+    }
+
+    public void Clear()
+    {
+        this.Head = null;
+        this.Tail = null;
+        this.Nodes.Clear();
+    }
+
+    public bool Exists(TKey key)
+        => this.Nodes.ContainsKey(key);
+
+    public bool TryGetValue(TKey key, out TValue? value)
+    {
+        var result = this.Nodes.TryGetValue(key, out var node);
+        value = result ? node!.Value : default;
+        return result;
+    }
+
+    public void Add(TValue value)
+    {
+        var key = this.KeySelector.Invoke(value);
+        var node = new LRUNode<TValue>(value);
+        if (!this.Nodes.TryAdd(key, node))
+            throw new InvalidOperationException($"Failed to add key of {key}");
+
+        if (this.Head is null)
+        {
+            this.Tail = this.Head = node;
+        }
+        else
+        {
+            this.Head.SetNext(node);
+            this.Head = node;
+        }
+    }
+
+    public void Remove(TValue value)
+    {
+        var key = this.KeySelector.Invoke(value);
+        if (!this.Nodes.TryRemove(key, out var node))
+            throw new KeyNotFoundException($"Not found key of {key}.");
+
+        if (this.Head == node) this.Head = node.Previous;
+        if (this.Tail == node) this.Tail = node.Next;
+
+        node.RemoveSelf();
+    }
+
+    public void BringToHead(TValue value)
+    {
+        var key = this.KeySelector.Invoke(value);
+        if (!this.Nodes.TryGetValue(key, out var node))
+            throw new KeyNotFoundException($"Not found key of {key}.");
+
+        if (this.Head == node) return;
+        node.RemoveSelf();
+        this.Head!.SetNext(node);
+        this.Head = node;
+    }
+}
---
+++ HackSystem.LRU/LRUNode.cs
@@ -0,0 +1,98 @@
+﻿namespace HackSystem.LRU;
+
+public class LRUNode<TValue>
+{
+    public LRUNode(TValue value)
+    {
+        this.Value = value;
+    }
+
+    public TValue Value { get; init; }
+
+    public bool IsHead { get => this.Next is null; }
+
+    public bool IsTail { get => this.Previous is null; }
+
+    public LRUNode<TValue>? Previous { get; protected set; }
+
+    public LRUNode<TValue>? Next { get; protected set; }
+
+    public void SetPrevious(LRUNode<TValue> previous)
+    {
+        if (this.IsTail)
+        {
+            this.Previous = previous;
+            previous.Next = this;
+        }
+        else
+        {
+            this.Previous!.Next = previous;
+            previous.Previous = this.Previous;
+            previous.Next = this;
+            this.Previous = previous;
+        }
+    }
+
+    public LRUNode<TValue>? SetNext(LRUNode<TValue> next)
+    {
+        if (this.IsHead)
+        {
+            this.Next = next;
+            next.Previous = this;
+        }
+        else
+        {
+            this.Next!.Previous = next;
+            next.Next = this.Next;
+            next.Previous = this;
+            this.Next = next;
+        }
+
+        return this;
+    }
+
+    public void RemoveSelf()
+    {
+        if (this.IsHead && this.IsTail) return;
+
+        if (!this.IsHead)
+        {
+            this.Next!.Previous = this.Previous;
+            this.Next = null;
+        }
+
+        if (!this.IsTail)
+        {
+            this.Previous!.Next = this.Next;
+            this.Previous = null;
+        }
+    }
+
+    public void RemovePrevious()
+    {
+        if (this.IsTail) return;
+
+        var previous = this.Previous!;
+        this.Previous = previous.Previous;
+        if (this.Previous is not null)
+        {
+            this.Previous.Next = this;
+        }
+        previous.Previous = null;
+        previous.Next = null;
+    }
+
+    public void RemoveNext()
+    {
+        if (this.IsHead) return;
+
+        var next = this.Next!;
+        this.Next = next.Next;
+        if (this.Next is not null)
+        {
+            this.Next.Previous = this;
+        }
+        next.Previous = null;
+        next.Next = null;
+    }
+}
