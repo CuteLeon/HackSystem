8a3fc34b86133ba102c40cc1763fbb35aca9ea5b
Leon Liu <leon.liu@hotmail.com>
2021-10-19 13:53:57 +0800

    Resort window Tier Index and re-render;

--- HackSystem.Web.ProgramSchedule.Infrastructure/IntermediaryHandler/WindowScheduleRequestHandler.cs
+++ HackSystem.Web.ProgramSchedule.Infrastructure/IntermediaryHandler/WindowScheduleRequestHandler.cs
@@ -1,4 +1,6 @@
-﻿using HackSystem.Web.ProgramSchedule.Intermediary;
+﻿using HackSystem.LRU;
+using HackSystem.Web.ProgramSchedule.Entity;
+using HackSystem.Web.ProgramSchedule.Intermediary;
 using HackSystem.Web.ProgramSchedule.IntermediaryHandler;
 
 namespace HackSystem.Web.ProgramSchedule.Infrastructure.IntermediaryHandler;
@@ -7,19 +9,52 @@ public class WindowScheduleRequestHandler : IWindowScheduleRequestHandler
 {
     public event IWindowScheduleRequestHandler.WindowScheduleHandler? OnWindowSchedule;
     private readonly ILogger<WindowScheduleRequestHandler> logger;
+    private readonly LRUContainer<string, ProgramWindowDetail> windowLRUContainer;
 
     public WindowScheduleRequestHandler(
         ILogger<WindowScheduleRequestHandler> logger)
     {
         this.logger = logger;
+        this.windowLRUContainer = new(window => window.WindowId, int.MaxValue);
     }
 
     public async Task<WindowScheduleResponse> Handle(WindowScheduleRequest request, CancellationToken cancellationToken)
     {
         this.logger.LogInformation($"Handle Window {request.ScheduleStates} request {request.ProgramWindowDetail.Caption} ...");
-        // TODO: Leon: LRU link
+        var scheduled = true;
+        switch (request.ScheduleStates)
+        {
+            case Abstractions.Enums.WindowScheduleStates.Schedule:
+                {
+                    scheduled = this.windowLRUContainer.BringToHead(request.ProgramWindowDetail);
+                    break;
+                }
+            case Abstractions.Enums.WindowScheduleStates.Launch:
+                {
+                    this.windowLRUContainer.Add(request.ProgramWindowDetail);
+                    break;
+                }
+            case Abstractions.Enums.WindowScheduleStates.Destory:
+                {
+                    this.windowLRUContainer.Remove(request.ProgramWindowDetail);
+                    break;
+                }
+            default:
+                break;
+        }
         this.logger.LogInformation($"Window {request.ScheduleStates} request handled, {request.ProgramWindowDetail.Caption}.");
-        this.OnWindowSchedule?.Invoke(request.ProgramWindowDetail);
-        return new WindowScheduleResponse(request.ScheduleStates, true);
+
+        if (scheduled)
+        {
+            // TODO: LEON: Get index from options;
+            // TODO: LEON: Just render z-index, and keep other datas;
+            foreach (var (window, index) in this.windowLRUContainer.GetValues().Select((window, index) => (window, index)))
+            {
+                window.TierIndex = 949 - index;
+                this.logger.LogWarning($"{window.Caption} => {window.TierIndex}");
+            }
+            this.OnWindowSchedule?.Invoke(request.ProgramWindowDetail);
+        }
+        return new WindowScheduleResponse(request.ScheduleStates, scheduled);
     }
 }
